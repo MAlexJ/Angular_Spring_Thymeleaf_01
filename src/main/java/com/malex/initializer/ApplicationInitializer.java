774e42e4389df832af40a0e9a88c0b3bacc79920
malexj <alexmaximovj@gmail.com>
2016-03-10 10:40:46 +0200

    added to the project: - WebConfig.class - HomeController.class - ApplicationInitializer.class

---
+++ src/main/java/com/malex/initializer/ApplicationInitializer.java
@@ -0,0 +1,27 @@
+package com.malex.initializer;
+
+import com.malex.config.WebConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    public final String DISPATCHER = "dispatcher";
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebConfig.class);
+        servletContext.addListener(new ContextLoaderListener(ctx));
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet(DISPATCHER, new DispatcherServlet(ctx));
+        servletRegistration.addMapping("/");
+        servletRegistration.setLoadOnStartup(1);
+    }
+}
